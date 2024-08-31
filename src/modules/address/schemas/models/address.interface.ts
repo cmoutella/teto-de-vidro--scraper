1df90a74b5fc9532af3614121c73e1d5d1f4fb3e
Carolina Moutella <ola@carolinamoutella.com>
2024-08-31 16:02:23 -0300

    refact(address): rewrite intertface to reuse lot and property interfaces

--- src/modules/address/schemas/models/address.interface.ts
+++ src/modules/address/schemas/models/address.interface.ts
@@ -1,21 +1,6 @@
-export interface InterfaceAddress {
-  lotName?: string;
-  street: string;
-  lotNumber: string;
-  postalCode?: string;
-  neighborhood: string;
-  city: string;
-  province: string;
-  country: string;
-  lotConvenience?: string[];
-  block?: string;
-  propertyNumber?: string;
-  size?: number;
-  rooms?: number;
-  bathrooms?: number;
-  parking?: number;
-  is_front?: boolean;
-  sun?: 'morning' | 'afternoon' | 'none';
-  condoPricing?: number;
-  propertyConvenience?: string[];
-}
+import { InterfaceLot } from './lot.interface';
+import { InterfaceProperty } from './property.interface';
+
+export interface InterfaceAddress
+  extends Omit<InterfaceProperty, 'id' | 'mainAddressId'>,
+    Omit<InterfaceLot, 'id'> {}
