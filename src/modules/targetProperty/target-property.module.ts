c178bd5598eb65eb8ad371df17a28a3fde1d835c
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-02-18 22:35:35 -0300

    feat(targetProperty): update hunt on create targetProperty + add swagger configuration

--- src/modules/targetProperty/target-property.module.ts
+++ src/modules/targetProperty/target-property.module.ts
@@ -8,19 +8,29 @@ import { TargetPropertyRepository } from './repositories/target-property.reposit
 import { TargetPropertyMongooseRepository } from './repositories/mongoose/target-property.mongoose.repository';
 import { TargetPropertyService } from './services/target-property.service';
 import { TargetPropertyController } from './controllers/target-property.controller';
+import { HuntService } from '../hunt/services/hunt-collection.service';
+import { HuntRepository } from '../hunt/repositories/hunt.repository';
+import { HuntMongooseRepository } from '../hunt/repositories/mongoose/hunt.mongoose.repository';
+import { Hunt, HuntSchema } from '../hunt/schemas/hunt.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: TargetProperty.name, schema: TargetPropertySchema },
     ]),
+    MongooseModule.forFeature([{ name: Hunt.name, schema: HuntSchema }]),
   ],
   providers: [
     {
       provide: TargetPropertyRepository,
       useClass: TargetPropertyMongooseRepository,
     },
+    {
+      provide: HuntRepository,
+      useClass: HuntMongooseRepository,
+    },
     TargetPropertyService,
+    HuntService,
   ],
   controllers: [TargetPropertyController],
 })
