154f99e34d226b8b2fd7deba1c11aeea785ba9a2
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-03-24 08:25:42 -0300

    patch(ZodValidationPipe): log validation errors

--- src/shared/pipe/zod-validation.pipe.ts
+++ src/shared/pipe/zod-validation.pipe.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, PipeTransform } from '@nestjs/common';
-import { ZodSchema } from 'zod';
+import { ZodError, ZodSchema } from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -7,8 +7,22 @@ export class ZodValidationPipe implements PipeTransform {
   transform(value: any) {
     try {
       const parsedValue = this.schema.parse(value);
+
+      if (parsedValue) {
+        console.log('# ZOD VALIDATION');
+        console.log('# Success: ok');
+        console.log('# # # # # # # #');
+      }
+
       return parsedValue;
     } catch (err) {
+      if (err instanceof ZodError) {
+        err.issues.forEach((issue) => {
+          console.log(
+            `Erro no caminho ${issue.path.join('.')}: ${issue.message}`,
+          );
+        });
+      }
       throw new BadRequestException('Validation failed');
     }
   }
