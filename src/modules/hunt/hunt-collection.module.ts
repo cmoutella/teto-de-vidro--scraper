7262864c0e0577d46a9451a984c1a46138e09497
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-04-24 13:03:44 -0300

    fix(hunt): module dependencies

--- src/modules/hunt/hunt-collection.module.ts
+++ src/modules/hunt/hunt-collection.module.ts
@@ -2,6 +2,7 @@ import { forwardRef, Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
 import { TargetPropertyCollectionModule } from '../targetProperty/target-property.module'
+import { UsersCollectionModule } from '../user/user.module'
 import { HuntController } from './controllers/hunt-collection.controller'
 import { HuntRepository } from './repositories/hunt.repository'
 import { HuntMongooseRepository } from './repositories/mongoose/hunt.mongoose.repository'
@@ -11,7 +12,8 @@ import { HuntService } from './services/hunt-collection.service'
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Hunt.name, schema: HuntSchema }]),
-    forwardRef(() => TargetPropertyCollectionModule)
+    forwardRef(() => TargetPropertyCollectionModule),
+    forwardRef(() => UsersCollectionModule)
   ],
   providers: [
     {
