66acedfa8e27c99e41e518539079666e5613acd2
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-05-14 10:17:37 -0300

    chore(comments): add service mocks

--- src/modules/comments/__tests__/__mocks__/index.ts
+++ src/modules/comments/__tests__/__mocks__/index.ts
@@ -1,3 +1,14 @@
+export const mockCommentService = {
+  createComment: jest.fn(),
+  getOneCommentById: jest.fn(),
+  getCommentsByTarget: jest.fn(),
+  getCommentsByTargetAndTopic: jest.fn(),
+  getCommentsByLot: jest.fn(),
+  getCommentsByProperty: jest.fn(),
+  updateComment: jest.fn(),
+  deleteComment: jest.fn()
+}
+
 export const mockCommentRepository = {
   createComment: jest.fn(),
   getOneCommentById: jest.fn(),
