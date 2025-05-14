972886b35976b3acbaa286c243f44ec0af2e9c77
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-05-14 09:52:14 -0300

    chore(amenity): organize test mocks

---
+++ src/modules/amenity/__tests__/__mocks__/index.ts
@@ -0,0 +1,8 @@
+export const mockAmenityRepository = {
+  createAmenity: jest.fn(),
+  createManyAmenities: jest.fn(),
+  updateAmenity: jest.fn(),
+  getOneAmenityById: jest.fn(),
+  getOneAmenityByLabel: jest.fn(),
+  deleteAmenity: jest.fn()
+}
