4ff186fee7e1ffa367e896d9879d71f3cda28552
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-05-14 10:04:04 -0300

    chore(amenity): add service mocks

--- src/modules/amenity/__tests__/__mocks__/index.ts
+++ src/modules/amenity/__tests__/__mocks__/index.ts
@@ -1,3 +1,13 @@
+export const mockAmenityService = {
+  createAmenity: jest.fn(),
+  createManyAmenities: jest.fn(),
+  updateAmenity: jest.fn(),
+  getOneAmenityById: jest.fn(),
+  getOneAmenityByLabel: jest.fn(),
+  getCompleteAmenitiesData: jest.fn(),
+  deleteAmenity: jest.fn()
+}
+
 export const mockAmenityRepository = {
   createAmenity: jest.fn(),
   createManyAmenities: jest.fn(),
