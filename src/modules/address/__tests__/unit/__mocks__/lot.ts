acd2b6562c796224a26e4409664d732a50828e60
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-05-14 10:03:03 -0300

    chore(address): organize test mocks

---
+++ src/modules/address/__tests__/unit/__mocks__/lot.ts
@@ -0,0 +1,9 @@
+export const mockLotRepository = {
+  getOneLotByAddress: jest.fn(),
+  getAllLotsByAddress: jest.fn(),
+  getAllLotsByCEP: jest.fn(),
+  getOneLot: jest.fn(),
+  createLot: jest.fn(),
+  updateLot: jest.fn(),
+  deleteLot: jest.fn()
+}
